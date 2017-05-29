e0e31f8be9adc8372a8db81b29facbfc7bedd069
Daniel Machado <daniel.machado@domrock.com.br>
2017-05-29 19:10:41 -0300

    Criação do servidor rest com a url Book

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+  .get(function(req, res) {
+    // var responseJson = { hello: 'This is my api' };
+    // res.json(responseJson);
+    Book.find(function(err, books) {
+      if (err)
+        console.log(err)
+      else
+        res.json(books);
+    });
+  });
+
+app.use('/api', bookRouter);
+
+app.get('/', function(req, res) {
+  res.send('Vindo a minha API!');
+});
+
+app.listen(port, function() {
+  console.log('Executando na Porta:' + port);
+});
